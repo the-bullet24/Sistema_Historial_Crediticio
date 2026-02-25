c51e6c2f7836e2a452dde105b5d9f0e0b75f419b
the-bullet24 <percyandersson12@gmail.com>
2026-02-25 00:22:14 -0500

    Se resuelve errores de subida de fronted a backend

--- src/main/java/com/financial/score/controller/TransaccionController.java
+++ src/main/java/com/financial/score/controller/TransaccionController.java
@@ -1,15 +1,15 @@
 package com.financial.score.controller;
 
-import com.financial.score.model.TransaccionDTO;
+import com.financial.score.model.Transaccion;
 import com.financial.score.service.TransaccionService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transacciones")
+@CrossOrigin(origins = "*") // Importante para conectar con tu React
 public class TransaccionController {
 
     private final TransaccionService service;
@@ -18,17 +18,5 @@ public class TransaccionController {
         this.service = service;
     }
 
-    @GetMapping
-    public List<TransaccionDTO> listar() {
 
-        return service.listar()
-                .stream()
-                .map(t -> new TransaccionDTO(
-                        t.getId(),
-                        t.getCodigoTransaccion(),
-                        t.getMontoTotal(),
-                        t.getEstadoPago()
-                ))
-                .toList();
-    }
 }
--- src/main/java/com/financial/score/service/TransaccionService.java
+++ src/main/java/com/financial/score/service/TransaccionService.java
@@ -1,21 +1,37 @@
 package com.financial.score.service;
 
+import com.financial.score.model.Producto;
 import com.financial.score.model.Transaccion;
+import com.financial.score.model.TransaccionDetalle;
+import com.financial.score.repository.ProductoRepository;
+import com.financial.score.repository.TransaccionDetalleRepository;
 import com.financial.score.repository.TransaccionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class TransaccionService {
 
     private final TransaccionRepository repository;
+    private final ProductoRepository productoRepository;
+    private final TransaccionDetalleRepository detalleRepository;
 
-    public TransaccionService(TransaccionRepository repository) {
+    // Inyectamos los nuevos repositorios necesarios
+    public TransaccionService(TransaccionRepository repository,
+                              ProductoRepository productoRepository,
+                              TransaccionDetalleRepository detalleRepository) {
         this.repository = repository;
+        this.productoRepository = productoRepository;
+        this.detalleRepository = detalleRepository;
     }
 
     public List<Transaccion> listar() {
         return repository.findAll();
     }
+
+
 }
